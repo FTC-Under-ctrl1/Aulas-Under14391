5353fadb23d5887c9453c4ce357371f71f95e131
Enzo <enzocoutinho81@gmail.com>
2021-03-02 15:06:38 -0300

    Names class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servo1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servo1.java
@@ -1,22 +1,18 @@
-package org.firstinspires.ftc.teamcode.servo;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name="Servo toggle", group="Linear Opmode")
-public class ServoToggle extends LinearOpMode {
+@TeleOp(name="Servo normal", group="Linear Opmode")
+public class Servo1 extends LinearOpMode {
 
     ElapsedTime runtime = new ElapsedTime();
 
     //Declaração dos Servos
     Servo servoDireita = null;
     Servo servoEsquerda = null;
-
-    //Variáveis para toggle
-    boolean antB = false;
-    boolean toggle = true;
 
     @Override
     public void runOpMode() {
@@ -45,18 +41,10 @@ public class ServoToggle extends LinearOpMode {
             //Referenciando a variável buttonA ao botão a do gamepad
             buttonA = gamepad1.a;
 
-            /* Este é um toggle tirado do reddit com uma miníma modificação nossa.
-             * https://www.reddit.com/r/FTC/comments/3z75ou/use_one_button_to_onoff/
-             * https://www.reddit.com/r/FTC/comments/enembp/programming_toggles/
-             */
-            if(buttonA && !antB) {
-                //Operador ternário, mais prático que dois ou + if's
-                servoPos = toggle ? 1 : 0;
-                toggle = !toggle;
-                antB = true;
-            } else if(!buttonA) {
-                antB = false;
-            }
+            //Manda o valor 1 apenas quando botão segurado
+            if(buttonA) {
+                servoPos = 1;
+            } else servoPos = 0;
 
             //Manda o valor para os servos
             servoDireita.setPosition(servoPos);
